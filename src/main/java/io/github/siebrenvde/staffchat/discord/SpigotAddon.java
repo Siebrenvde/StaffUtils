5f5bf1b53b681c31a27b589294917d4626ab8513
Siebrenvde <sieb@siebrenvde.dev>
2022-08-20 00:40:39 +0200

    Replace onLoad with onReady - #3, #4

--- src/main/java/io/github/siebrenvde/staffchat/discord/SpigotAddon.java
+++ src/main/java/io/github/siebrenvde/staffchat/discord/SpigotAddon.java
@@ -23,7 +23,7 @@ public class SpigotAddon extends SimpleAddon {
     }
 
     @Override
-    public void onLoad(DiscordBot bot) {
+    public void onReady(DiscordBot bot) {
         this.bot = bot;
         prefix = bot.getCommandPrefix();
         enableCommands();
