6dc037f1f4291d99f6331466ad94361762b89e2a
Siebrenvde <siebren@siebrenvde.dev>
2024-09-25 04:19:29 +0200

    Register Spicord addon

--- common/src/main/java/dev/siebrenvde/staffchat/common/StaffChat.java
+++ common/src/main/java/dev/siebrenvde/staffchat/common/StaffChat.java
@@ -1,6 +1,7 @@
 package dev.siebrenvde.staffchat.common;
 
 import dev.siebrenvde.staffchat.common.spicord.Addon;
+import org.spicord.SpicordLoader;
 
 public class StaffChat {
 
@@ -10,6 +11,9 @@ public class StaffChat {
     public StaffChat(MinecraftPlugin plugin) {
         this.plugin = plugin;
         this.addon = new Addon(plugin);
+        SpicordLoader.addStartupListener(spicord -> {
+            spicord.getAddonManager().registerAddon(addon);
+        });
     }
 
     public MinecraftPlugin getPlugin() { return plugin; }
