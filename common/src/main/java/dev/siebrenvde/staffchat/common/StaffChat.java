b9d5f6dca05cd2253082711356125f0bca997cd0
Siebrenvde <siebren@siebrenvde.dev>
2024-09-25 04:33:50 +0200

    Register command in StaffChat.java

--- common/src/main/java/dev/siebrenvde/staffchat/common/StaffChat.java
+++ common/src/main/java/dev/siebrenvde/staffchat/common/StaffChat.java
@@ -1,6 +1,8 @@
 package dev.siebrenvde.staffchat.common;
 
+import dev.siebrenvde.staffchat.common.commands.StaffChatCommand;
 import dev.siebrenvde.staffchat.common.spicord.Addon;
+import org.incendo.cloud.CommandManager;
 import org.spicord.SpicordLoader;
 
 public class StaffChat {
@@ -8,12 +10,13 @@ public class StaffChat {
     private final MinecraftPlugin plugin;
     private final Addon addon;
 
-    public StaffChat(MinecraftPlugin plugin) {
+    public <C> StaffChat(MinecraftPlugin plugin, CommandManager<C> commandManager) {
         this.plugin = plugin;
         this.addon = new Addon(plugin);
         SpicordLoader.addStartupListener(spicord -> {
             spicord.getAddonManager().registerAddon(addon);
         });
+        StaffChatCommand.register(commandManager, plugin);
     }
 
     public MinecraftPlugin getPlugin() { return plugin; }
