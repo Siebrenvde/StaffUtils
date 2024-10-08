d0246a9f41598745a13014a9c8f36b1dbdc32f99
Siebrenvde <siebren@siebrenvde.dev>
2024-10-08 16:54:23 +0200

    Add more javadoc

--- common/src/main/java/dev/siebrenvde/staffchat/common/minecraft/MinecraftCommandSender.java
+++ common/src/main/java/dev/siebrenvde/staffchat/common/minecraft/MinecraftCommandSender.java
@@ -1,13 +1,28 @@
 package dev.siebrenvde.staffchat.common.minecraft;
 
+import net.kyori.adventure.text.Component;
+
 public interface MinecraftCommandSender {
 
+    /**
+     * {@return the name of the command sender}
+     */
     String getName();
 
+    /**
+     * {@return the display name of the command sender}
+     */
     String getDisplayName();
 
+    /**
+     * {@return the name of the server the command sender is connected to}
+     */
     String getServerName();
 
-    void sendMessage(String message);
+    /**
+     * Sends a message to the command sender
+     * @param message the message to send
+     */
+    void sendMessage(Component message);
 
 }
--- common/src/main/java/dev/siebrenvde/staffchat/common/minecraft/MinecraftPlugin.java
+++ common/src/main/java/dev/siebrenvde/staffchat/common/minecraft/MinecraftPlugin.java
@@ -4,14 +4,14 @@ import net.kyori.adventure.text.Component;
 
 public interface MinecraftPlugin {
 
-    /***
+    /**
      * Broadcasts a message to all online players with the specified permission
      * @param message The message to broadcast
      * @param permission The required permission
      */
     void broadcast(Component message, String permission);
 
-    /***
+    /**
      * Returns an instance of {@link MinecraftCommandSender} corresponding to the provided command sender
      * @param sender The command sender
      * @return An instance of {@link MinecraftCommandSender}
