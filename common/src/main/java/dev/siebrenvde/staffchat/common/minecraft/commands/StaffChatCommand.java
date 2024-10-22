f082e5c749a442222f17708c9b95135a9f2ee3fc
Siebrenvde <siebren@siebrenvde.dev>
2024-10-22 16:46:38 +0200

    Implement toggling StaffChat

--- common/src/main/java/dev/siebrenvde/staffchat/common/minecraft/commands/StaffChatCommand.java
+++ common/src/main/java/dev/siebrenvde/staffchat/common/minecraft/commands/StaffChatCommand.java
@@ -4,11 +4,17 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import dev.siebrenvde.staffchat.common.StaffChat;
 import dev.siebrenvde.staffchat.common.messages.Messages;
-import dev.siebrenvde.staffchat.common.minecraft.BrigadierCommandManager;
-import dev.siebrenvde.staffchat.common.minecraft.CommandSender;
-import dev.siebrenvde.staffchat.common.minecraft.ServerPlatform;
+import dev.siebrenvde.staffchat.common.minecraft.*;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class StaffChatCommand extends BaseCommand {
+
+    public static final List<UUID> ENABLED_PLAYERS = new ArrayList<>();
 
     public StaffChatCommand() {
         super(
@@ -24,6 +30,16 @@ public class StaffChatCommand extends BaseCommand {
         return manager.literal(getName())
             .requires(sender -> platform.getCommandSender(sender).hasPermission(getPermission()))
             .executes(ctx -> {
+                if(
+                    platform.getServerType() == ServerPlatform.ServerType.VELOCITY
+                    && CommandSender.of(ctx.getSource()) instanceof ProxyPlayer player
+                    && player.getProtocolVersion() >= 760
+                    /* Cannot deny signed messages on Velocity with 1.19.1+ clients */
+                ) {
+                    player.sendMessage(Component.text("Toggling StaffChat is not supported by your client", NamedTextColor.RED));
+                    StaffChat.LOGGER.warn("Toggling StaffChat is not supported when using a 1.19.1+ client with Velocity");
+                    return 1;
+                }
                 executeToggle(platform.getCommandSender(ctx.getSource()));
                 return 1;
             })
@@ -47,10 +63,25 @@ public class StaffChatCommand extends BaseCommand {
     }
 
     private void executeToggle(CommandSender sender) {
-        // TODO: Implement
+        if(!(sender instanceof Player player)) {
+            sender.sendMessage(Component.text("Only players can toggle StaffChat", NamedTextColor.RED));
+            return;
+        }
+        UUID uuid = player.getUniqueId();
+        if(!ENABLED_PLAYERS.contains(uuid)) {
+            ENABLED_PLAYERS.add(uuid);
+            player.sendMessage(
+                Component.text("Enabled StaffChat", NamedTextColor.GREEN)
+                    .appendNewline()
+                    .append(Component.text("Any message you send will be sent to the staff chat"))
+            );
+        } else {
+            ENABLED_PLAYERS.remove(uuid);
+            player.sendMessage(Component.text("Disabled StaffChat", NamedTextColor.RED));
+        }
     }
 
-    private void executeSendMessage(CommandSender sender, String message) {
+    public static void executeSendMessage(CommandSender sender, String message) {
         StaffChat.getPlatform().broadcast(
             Messages.staffChat().serverFromServer(sender, message),
             "staffchat.see"
---
+++ common/src/main/java/dev/siebrenvde/staffchat/common/minecraft/listeners/EventListeners.java
@@ -0,0 +1,14 @@
+package dev.siebrenvde.staffchat.common.minecraft.listeners;
+
+import dev.siebrenvde.staffchat.common.minecraft.Player;
+import dev.siebrenvde.staffchat.common.minecraft.commands.StaffChatCommand;
+
+public class EventListeners {
+
+    public static boolean chatListener(Player player, String message) {
+        if(!StaffChatCommand.ENABLED_PLAYERS.contains(player.getUniqueId())) return false;
+        StaffChatCommand.executeSendMessage(player, message);
+        return true;
+    }
+
+}
