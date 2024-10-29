62253546c14f4b1594673ec13d325dd4c295899a
Siebrenvde <siebren@siebrenvde.dev>
2024-10-29 23:17:35 +0100

    Prevent messages being sent when bot is not ready

--- common/src/main/java/dev/siebrenvde/staffchat/common/spicord/Addon.java
+++ common/src/main/java/dev/siebrenvde/staffchat/common/spicord/Addon.java
@@ -6,10 +6,15 @@ import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import org.spicord.api.addon.SimpleAddon;
 import org.spicord.bot.DiscordBot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Addon extends SimpleAddon {
 
-    private JDA jda;
+    private boolean isReady = false;
     private TextChannel staffChannel;
+
+    private final List<String> messageQueue = new ArrayList<>();
 
     public Addon() {
         super("StaffChat", "staffchat", "Siebrenvde");
@@ -17,13 +22,19 @@ public class Addon extends SimpleAddon {
 
     @Override
     public void onReady(DiscordBot bot) {
-        jda = bot.getJda();
+        JDA jda = bot.getJda();
         if(Config.CONFIG.staffChannel.isEmpty()) throw new RuntimeException("Staff channel is empty");
         staffChannel = jda.getTextChannelById(Config.CONFIG.staffChannel);
         jda.addEventListener(new MessageListener(this));
+        isReady = true;
+        staffChannel.sendMessage(String.join("\n", messageQueue)).queue();
     }
 
     public void sendMessage(String message) {
+        if(!isReady) {
+            messageQueue.add(message);
+            return;
+        }
         staffChannel.sendMessage(message).queue();
     }
 
