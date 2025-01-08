743181744ec94999ab9b36a39c5666b566171223
Siebrenvde <siebren@siebrenvde.dev>
2025-01-08 22:05:38 +0100

    Remove message queue

--- common/src/main/java/dev/siebrenvde/staffchat/spicord/Addon.java
+++ common/src/main/java/dev/siebrenvde/staffchat/spicord/Addon.java
@@ -6,15 +6,10 @@ import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import org.spicord.api.addon.SimpleAddon;
 import org.spicord.bot.DiscordBot;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Addon extends SimpleAddon {
 
     private boolean isReady = false;
     private TextChannel staffChannel;
-
-    private final List<String> messageQueue = new ArrayList<>();
 
     public Addon() {
         super("StaffChat", "staffchat", "Siebrenvde");
@@ -27,15 +22,10 @@ public class Addon extends SimpleAddon {
         staffChannel = jda.getTextChannelById(Config.CONFIG.staffChannel);
         jda.addEventListener(new MessageListener(this));
         isReady = true;
-        if(!messageQueue.isEmpty()) staffChannel.sendMessage(String.join("\n", messageQueue)).queue();
     }
 
     public void sendMessage(String message) {
-        if(!isReady) {
-            messageQueue.add(message);
-            return;
-        }
-        staffChannel.sendMessage(message).queue();
+        if(isReady) staffChannel.sendMessage(message).queue();
     }
 
     public TextChannel getStaffChannel() { return staffChannel; }
