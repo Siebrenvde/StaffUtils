0fa5d2064227ac975d499ff08a158b4029798094
Siebrenvde <siebren@siebrenvde.dev>
2025-03-03 03:08:13 +0100

    Add version subcommand

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    id("net.kyori.indra.git") version "3.1.3"
+    id("net.kyori.blossom") version "2.1.0"
+    id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.10"
+}
+
 dependencies {
     implementation("dev.siebrenvde:ConfigLib:0.2.0")
     compileOnly("com.github.Spicord.Spicord:spicord-common:5.6.0")
@@ -9,4 +15,12 @@ dependencies {
     implementation("net.kyori:adventure-text-serializer-plain:4.18.0")
     implementation("net.kyori:adventure-text-serializer-legacy:4.18.0")
     implementation("org.jspecify:jspecify:1.0.0")
+}
+
+sourceSets.main {
+    blossom.javaSources {
+        property("version", project.version.toString())
+        property("gitCommit", indraGit.commit()?.name())
+        property("gitBranch", indraGit.branchName())
+    }
 }
--- common/src/main/java/dev/siebrenvde/staffutils/commands/StaffUtilsCommand.java
+++ common/src/main/java/dev/siebrenvde/staffutils/commands/StaffUtilsCommand.java
@@ -1,11 +1,17 @@
 package dev.siebrenvde.staffutils.commands;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import dev.siebrenvde.staffutils.StaffUtils;
 import dev.siebrenvde.staffutils.api.command.BaseCommand;
 import dev.siebrenvde.staffutils.config.Config;
 import dev.siebrenvde.staffutils.messages.Messages;
+import dev.siebrenvde.staffutils.util.BuildParameters;
 import dev.siebrenvde.staffutils.util.Permissions;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.jspecify.annotations.NullMarked;
+
+import static net.kyori.adventure.text.Component.text;
 
 @NullMarked
 public class StaffUtilsCommand<C> extends BaseCommand<C> {
@@ -18,6 +24,30 @@ public class StaffUtilsCommand<C> extends BaseCommand<C> {
     public LiteralArgumentBuilder<C> builder() {
         return literal(getName())
             .requires(hasPermission(getRootPermission()))
+            .then(literal("version")
+                .executes(withSender((ctx, sender) -> {
+                    sender.sendMessage(
+                        text()
+                            .append(text("StaffUtils "))
+                            .append(text("("))
+                            .append(StaffUtils.getPlatform().getServerType().displayName())
+                            .append(text(")"))
+                            .append(text(" version "))
+                            .append(text(BuildParameters.VERSION, NamedTextColor.YELLOW))
+                            .appendSpace()
+                            .append(
+                                text()
+                                    .append(text("("))
+                                    .append(text(BuildParameters.GIT_BRANCH, NamedTextColor.AQUA))
+                                    .append(text("@"))
+                                    .append(text(BuildParameters.GIT_COMMIT.substring(0, 7), NamedTextColor.GREEN))
+                                    .append(text(")"))
+                                    .clickEvent(ClickEvent.openUrl("https://github.com/Siebrenvde/StaffUtils/commit/" + BuildParameters.GIT_COMMIT))
+                            )
+                            .build()
+                    );
+                }))
+            )
             .then(literal("reload")
                 .requires(hasPermission(Permissions.COMMAND_STAFFUTILS_RELOAD))
                 .executes(withSender((ctx, sender) -> {
